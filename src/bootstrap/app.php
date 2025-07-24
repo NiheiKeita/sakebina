2dfe9ef57fcb66cbe3c5e152604a26fa55571925
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-07-25 00:58:12 +0900

    fix

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -111,6 +111,7 @@ $app->register(Illuminate\Foundation\Providers\FoundationServiceProvider::class)
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
